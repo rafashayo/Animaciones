989694a27a3eb913b5e6714190b3d0b83f0fdf77
rafashayo <49433616@est.ort.edu.ar>
2025-08-12 10:40:52 -0300

    f

---
+++ Assets/ZombieController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+ 	anim.SetBool("IsWalking", false);       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	if(Input.GetKeyDown(KeyCode.W))
+	{
+		anim.SetBool("IsWalking", false);
+	}
+	if(Input.GetKeyUp(KeyCode.W))
+	{
+		anim.SetBool("IsWalking", true);
+	}   
+    }
+}
